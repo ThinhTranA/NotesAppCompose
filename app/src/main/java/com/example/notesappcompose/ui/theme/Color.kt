c9e7b6007fd959f92fd0a4518ed2bced927ba6d4
Thinh Tran <thinhtran@outlook.com.au>
2021-08-23 22:02:16 +0800

    Add App Drawer Header and Note components.

--- app/src/main/java/com/example/notesappcompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/notesappcompose/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.example.notesappcompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val mGreen = Color(0xFF06837)
+val mGreenDark = Color(0xFF004012)
+val mRed = Color(0xFFC75F00)
--- app/src/main/java/com/example/notesappcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/notesappcompose/ui/theme/Theme.kt
@@ -5,43 +5,36 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val LightThemeColors = lightColors(
+    primary = mGreen,
+    primaryVariant = mGreenDark,
+    secondary = mGreen
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val DarkThemeColors = lightColors(
+    primary = mGreen,
+    primaryVariant = mGreenDark,
+    secondary = mGreen
 )
 
+/**
+ * Responsible for switching color palette for dark and light theme.
+ */
 @Composable
-fun NotesAppComposeTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
-) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun NotesAppComposeTheme(content: @Composable () -> Unit) {
+    val isDarkThemeEnabled = isSystemInDarkTheme() || JetNotesThemeSettings.isDarkThemeEnabled
+    val colors = if (isDarkThemeEnabled) DarkThemeColors else LightThemeColors
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    MaterialTheme(colors = colors, content = content)
+}
+
+/**
+ * Allows changing between light and a dark theme from the app's settings.
+ */
+object JetNotesThemeSettings {
+    var isDarkThemeEnabled by mutableStateOf(false)
 }
