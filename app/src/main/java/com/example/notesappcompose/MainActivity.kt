8f32e175ecd91b34457a55cbeb5370a125834eac
Thinh Tran <thinhtran@outlook.com.au>
2021-08-24 23:53:25 +0800

    Display App Drawer Component in MainActivity

--- app/src/main/java/com/example/notesappcompose/MainActivity.kt
+++ app/src/main/java/com/example/notesappcompose/MainActivity.kt
@@ -3,36 +3,41 @@ package com.example.notesappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.notesappcompose.routing.Screen
+import com.example.notesappcompose.ui.components.AppDrawer
+import com.example.notesappcompose.ui.components.Note
 import com.example.notesappcompose.ui.theme.NotesAppComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NotesAppComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                val coroutineScope = rememberCoroutineScope()
+                val scaffoldState: ScaffoldState = rememberScaffoldState()
+
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    drawerContent = {
+                        AppDrawer(
+                            currentScreen = Screen.Notes,
+                            closeDrawerAction = {
+                                coroutineScope.launch {
+                                    scaffoldState.drawerState.close()
+                                }
+                            }
+                        )
+                    },
+                    content = {
+                        Note()
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NotesAppComposeTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/notesappcompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/notesappcompose/ui/theme/Color.kt
@@ -2,6 +2,6 @@ package com.example.notesappcompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val mGreen = Color(0xFF06837)
+val mGreen = Color(0xFF006837)
 val mGreenDark = Color(0xFF004012)
 val mRed = Color(0xFFC75F00)
