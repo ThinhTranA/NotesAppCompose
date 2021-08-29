3af9921b0beac86bf54b17aeb4633071ab833331
Thinh Tran <thinhtran@outlook.com.au>
2021-08-29 21:15:59 +0800

    Add Savenote screen and Color Picker.

---
+++ app/src/main/java/com/example/notesappcompose/domain/model/ColorModel.kt
@@ -0,0 +1,14 @@
+package com.example.notesappcompose.domain.model
+
+import com.example.notesappcompose.data.model.ColorDbModel
+
+data class ColorModel(
+    val id: Long,
+    val name: String,
+    val hex: String
+) {
+
+    companion object {
+        val DEFAULT = with(ColorDbModel.DEFAULT_COLOR) { ColorModel(id, name, hex) }
+    }
+}
